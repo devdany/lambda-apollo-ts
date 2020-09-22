b88ca3604086c8fa13bfc39afb5da10953ad2b5a
Dany <dany@Danys-MacBookon-Goodoc.local>
2020-09-22 12:13:38 +0900

    remove build

--- .build/src/graphql/types/mutation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = `
-  type Mutation {
-    setHello(hello: String): String
-    setUser(email: String!, password: String!, username: String!): User!
-  }
-`;
-//# sourceMappingURL=mutation.js.map
--- .build/src/graphql/types/query.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = `
-  type Query {
-    getHello: String
-    getUser(userId: Int!): User!
-  }
-`;
-//# sourceMappingURL=query.js.map
